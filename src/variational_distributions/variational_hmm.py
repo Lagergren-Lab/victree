835b539a8337b76c57ab4491ec1d4bc3250a1324
Vittorio Zampinetti <zampinetti@gmail.com>
2022-10-20 18:24:08 +0200

    update completed

--- src/variational_distributions/variational_hmm.py
+++ src/variational_distributions/variational_hmm.py
@@ -17,12 +17,33 @@ class CopyNumberHmm(VariationalDistribution):
 
         self.single_filtering_probs = torch.empty((n_nodes, chain_length, n_states))
         self.couple_filtering_probs = torch.empty((n_nodes, chain_length, n_states, n_states))
+
+        self.eta1 = torch.empty((n_nodes, chain_length, n_states))
+        self.eta2 = torch.empty((n_nodes, chain_length, n_states, n_states))
         super().__init__()
 
-    def update(self):
-        self.update_CAVI()
+    def initialize():
+        # TODO: implement initialization of parameters
+        pass
 
-    def update_CAVI(self, Y, T: nx.DiGraph, mu, sigma,):
+    def log_density(self, copy_numbers: torch.Tensor, nodes: list = []) -> float:
+        # compute probability of a copy number sequence over a set of nodes
+        # if the nodes are not specified, whole q_c is evaluated (all nodes)
+        # copy_numbers has shape (nodes, chain_length)
+        # TODO: it's more complicated than expected, fixing it later
+        if len(nodes):
+            assert(copy_numbers.shape[0] == len(nodes))
+            pass
+
+        else:
+            pass
+
+
+    def update(self, obs: torch.Tensor, 
+            q_t: VariationalDistribution,
+            q_eps: VariationalDistribution,
+            q_z: VariationalDistribution,
+            q_mutau: VariationalDistribution) -> tuple[torch.Tensor, torch.Tensor]:
         """
         log q*(C) += ( E_q(mu)q(sigma)[rho_Y(Y^u, mu, sigma)] + E_q(T)[E_{C^p_u}[eta(C^p_u, epsilon)] +
         + Sum_{u,v in T} E_{C^v}[rho_C(C^v,epsilon)]] ) dot T(C^u)
@@ -31,13 +52,25 @@ class CopyNumberHmm(VariationalDistribution):
         HMM and simplified expected value over the natural parameter.
         :return:
         """
-        n_nodes = T.number_of_nodes()
-        T_C = torch.zeros((n_nodes, ))
-        eta_C = torch.zeros((n_nodes, ))
-        eta_C += self.E_mu_sigma_of_rho_Y(mu, sigma, Y)
-        eta_C += self.E_mu_sigma_of_rho_Y(mu, sigma, Y)
-        return torch.einsum()
+        new_eta1 = torch.zeros((self.n_nodes, self.chain_length, self.n_states))
+        new_eta2 = torch.zeros((self.n_nodes, self.chain_length, self.n_states, self.n_states)) 
 
+        for tree in q_t.get_trees_sample():
+            new_eta1, new_eta2 = self.exp_eta(obs, tree, q_eps, q_z, q_mutau)
+            new_eta1 += torch.einsum('', exp_alpha1)
+
+            # compute all alpha quantities
+            exp_alpha1, exp_alpha2 = self.exp_alpha(q_eps)
+            for u in range(self.n_nodes):
+                # for each node, get the children
+                children = [w for w in tree.successors(u)]
+                # sum on each node's update all children alphas
+                new_eta1[u, :, :] += torch.einsum('wmi->mi', exp_alpha1[children, :, :])
+                new_eta2[u, :, :, :] += torch.einsum('wmij->mij', exp_alpha2[children, :, :, :])
+            
+        return new_eta1, new_eta2
+
+    # TODO: move to mutau distribution class
     def E_mu_sigma_of_rho_Y(self, mu, sigma, Y, C):
         """
         Evaluates the
@@ -52,7 +85,7 @@ class CopyNumberHmm(VariationalDistribution):
         E_mu_sigma_p_Y = torch.einsum('ij', 'ij-> ij', mu, p_Y)
         return E_mu_sigma_p_Y
 
-    def exp_eta(self, tree: nx.DiGraph, 
+    def exp_eta(self, obs: torch.Tensor, tree: nx.DiGraph, 
             q_eps: VariationalDistribution,
             q_z: VariationalDistribution,
             q_mutau: VariationalDistribution) -> tuple[torch.Tensor, torch.Tensor]:
@@ -94,7 +127,7 @@ class CopyNumberHmm(VariationalDistribution):
         # TODO: define q_z and q_mutau functions
         e_eta1[inner_nodes, 1:, :] = torch.einsum('nv,nmi->vmi',
                 q_z.exp_assignment,
-                q_mutau.exp_log_emission())
+                q_mutau.exp_log_emission(obs))
 
         # eta_2_kappa(m, i, i')
         e_eta2[inner_nodes, :, :, :] = torch.einsum('pmjk,hikj->pmih',
@@ -128,6 +161,7 @@ class CopyNumberHmm(VariationalDistribution):
         return e_alpha1, e_alpha2
 
     def mc_filter(self, u, m, i: Union[int, tuple[int, int]]):
+        # TODO: implement/import forward-backward starting from eta params
         if isinstance(i, int):
             return self.single_filtering_probs[u, m, i]
         else:
