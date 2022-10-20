ec3960ed8c8c13d67879408c55e50a62584d92e1
Vittorio Zampinetti <zampinetti@gmail.com>
2022-10-20 16:54:33 +0200

    alpha expectation

--- src/variational_distributions/variational_hmm.py
+++ src/variational_distributions/variational_hmm.py
@@ -27,8 +27,8 @@ class CopyNumberHmm(VariationalDistribution):
         log q*(C) += ( E_q(mu)q(sigma)[rho_Y(Y^u, mu, sigma)] + E_q(T)[E_{C^p_u}[eta(C^p_u, epsilon)] +
         + Sum_{u,v in T} E_{C^v}[rho_C(C^v,epsilon)]] ) dot T(C^u)
 
-        CAVI update based on the dot product of the sufficient statistic of the HMM and simplified expected value over
-        the natural parameter.
+        CAVI update based on the dot product of the sufficient statistic of the 
+        HMM and simplified expected value over the natural parameter.
         :return:
         """
         n_nodes = T.number_of_nodes()
@@ -85,35 +85,47 @@ class CopyNumberHmm(VariationalDistribution):
         e_eta2 = torch.zeros((self.n_nodes, self.chain_length, self.n_states, self.n_states))
 
         # eta_1_iota(1, i)
-        e_eta1[inner_nodes, 1, :] += torch.einsum('pj,ij->pi',
+        e_eta1[inner_nodes, 0, :] = torch.einsum('pj,ij->pi',
                 self.single_filtering_probs[
-                    [tree.predecessors(u)[0] for u in inner_nodes], 1, :],
+                    [tree.predecessors(u)[0] for u in inner_nodes], 0, :],
+                # TODO: check if q_eps.h_eps0 can return a 2d tensor directly
                 torch.tensor([q_eps.h_eps0(i, j) for (i, j) in iter_pair_states(self.n_states)]))
         # eta_1_iota(m, i)
         # TODO: define q_z and q_mutau functions
-        e_eta1[inner_nodes, 1:, :] += torch.einsum('nv,nmi->vmi',
-                q_z.assignment_probs,
+        e_eta1[inner_nodes, 1:, :] = torch.einsum('nv,nmi->vmi',
+                q_z.exp_assignment,
                 q_mutau.exp_log_emission())
 
         # eta_2_kappa(m, i, i')
-        e_eta2[inner_nodes, :, :, :] += torch.einsum('pmjk,ihjk->pmih',
+        e_eta2[inner_nodes, :, :, :] = torch.einsum('pmjk,hikj->pmih',
                 self.couple_filtering_probs,
                 q_eps.exp_zipping())
 
         # natural parameters for root node are fixed to healthy state
-        e_eta1[root, 1, 2] = 1
+        e_eta1[root, 0, 2] = 1
         e_eta1[root, :, :, 2] = 1
 
         return e_eta1, e_eta2
             
 
-    def exp_alpha(self, u, m, i: Union[int, tuple[int, int]], q_z = None, q_mutau = None):
+    def exp_alpha(self, q_eps: VariationalDistribution) -> tuple[torch.Tensor, torch.Tensor]:
+
+        e_alpha1 = torch.zeros((self.n_nodes, self.chain_length, self.n_states))
+        e_alpha2 = torch.zeros((self.n_nodes, self.chain_length, self.n_states, self.n_states))
+
         # alpha_iota(m, i)
-        if isinstance(i, int):
-            pass
+        e_alpha1 = torch.einsum('wmj,ji->wmi',
+                self.single_filtering_probs,
+                # here q_eps.h_eps0 returns a tensor directly
+                q_eps.h_eps0())
+
         # alpha_kappa(m, i, i')
-        else:
-            pass
+        # similar to eta2 but with inverted indices in zipping
+        e_alpha2 = torch.einsum('wmjk,kjhi->wmi',
+                self.couple_filtering_probs,
+                q_eps.exp_zipping())
+
+        return e_alpha1, e_alpha2
 
     def mc_filter(self, u, m, i: Union[int, tuple[int, int]]):
         if isinstance(i, int):
