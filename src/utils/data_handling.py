9c6d884d8ce509aa361468ff06ae60bc943c3326
Vittorio Zampinetti <zampinetti@gmail.com>
2022-11-21 16:16:46 +0100

    test: data format read test

--- src/utils/data_handling.py
+++ src/utils/data_handling.py
@@ -1,6 +1,5 @@
 from typing import List, Tuple
 import torch
-import numpy as np
 
 # TODO: read data and make config read data input specs
 def read_sc_data(file_path: str) -> Tuple[List, List, torch.Tensor]:
@@ -10,28 +9,17 @@ def read_sc_data(file_path: str) -> Tuple[List, List, torch.Tensor]:
         obs_lst = []
         nlines = 0
         for line in f:
+            nlines += 1
             lspl = line.strip().split(" ")
             gene_ids.append(lspl[0])
-            obs_lst.append(list(map(int, lspl[1:])))
-            nlines += 1
+            new_obs = list(map(int, lspl[1:]))
+            if len(cell_names) != len(new_obs):
+                err_msg = f"file format not valid: {file_path} has \
+                {len(cell_names)} cells and {len(new_obs)} \
+                reads at line {nlines} (gene_id {lspl[0]})"
+                raise RuntimeError(err_msg)
 
-    obs = torch.tensor(obs_lst)
+            obs_lst.append(new_obs)
 
-
-        # # name_matrix = np.genfromtxt(f, dtype=None,
-        # #                          delimiter=" ", encoding=None)
-        # name_matrix = np.loadtxt(f, dtype={'names': ('gene_id',) + tuple(cell_names), 
-        #                                    'formats': ('U25',) + ('u8',) * len(cell_names)},
-        #                          delimiter=" ", ndmin=2)
-        # gene_ids = name_matrix['gene_id'].tolist()
-        # obs = torch.tensor(name_matrix[1:])
-
-    return cell_names, gene_ids, obs
-
-            
-
-
-
-
-
-
+        obs = torch.tensor(obs_lst)
+        return cell_names, gene_ids, obs
