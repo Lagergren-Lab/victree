01fcc892f8af126105883bd0f0718038ddf8a8ee
Harald <harald.melin@gmail.com>
2022-12-05 11:20:50 +0100

    some change

--- tests/tests_qT.py
+++ tests/tests_qT.py
@@ -3,6 +3,8 @@ import unittest
 import networkx as nx
 import torch
 
+from variational_distributions.variational_hmm import CopyNumberHmm
+from variational_distributions.q_epsilon import qEpsilon
 from utils import tree_utils
 from tests import utils_testing
 from utils.config import Config
@@ -12,15 +14,15 @@ from variational_distributions.q_T import q_T
 class qTTestCase(unittest.TestCase):
 
     def setUp(self) -> None:
-        config = Config(wis_sample_size = 5)
-        self.q_T = q_T(config)
+        self.config = Config(wis_sample_size = 5)
+        self.q_T = q_T(self.config)
 
     def test_q_T_running_for_two_simple_Ts_random_qC(self):
         M = 20
         A = 5
         T_list, q_C_pairwise_marginals = utils_testing.get_two_simple_trees_with_random_qCs(M, A)
-        q_C = torch.tensor([1.0])
-        q_epsilon = torch.tensor([2.0])
+        q_C = CopyNumberHmm(config=self.config)
+        q_epsilon = qEpsilon(config=self.config)
         # Act
         log_q_T = self.q_T.update_CAVI(T_list, q_C_pairwise_marginals, q_C, q_epsilon)
 
