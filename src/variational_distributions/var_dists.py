Added epsilon to qC entropy calculation to avoid Nan from log zero probability states = -inf. Nan Elbo solved for small and large fixed trees but not main.

src/variational_distributions/var_dists.py
@@ -76,22 +76,28 @@ class qC(VariationalDistribution):
         return init_entropy + transitions_entropy
 
     def marginal_entropy(self):
-        return -torch.einsum("kmi, kmi ->", self.single_filtering_probs, torch.log(self.single_filtering_probs))
+        eps = 0.00001  # To avoid log_marginals having entries of -inf
+        log_marginals = torch.log(self.single_filtering_probs + eps)
+        return -torch.einsum("kmi, kmi ->", self.single_filtering_probs, log_marginals)
 
-    def elbo(self, T_list, w_T_list, q_eps: Union['qEpsilon', 'qEpsilonMulti']) -> float:
-        # unique_arcs, unique_arcs_count = tree_utils.get_unique_edges(T_list, self.config.n_nodes)
-        # for (a, a_count) in zip(unique_arcs, unique_arcs_count):
-        # alpha_1, alpha_2 = self.exp_alpha()
+    def cross_entropy(self, T_list, w_T_list, q_eps: Union['qEpsilon', 'qEpsilonMulti']) -> float:
         E_T = 0
         L = len(T_list)
         normalising_weight = torch.logsumexp(torch.tensor(w_T_list), dim=0)
         for l in range(L):
             alpha_1, alpha_2 = self.exp_alpha(T_list[l], q_eps)
-            cross_ent_pos_0 = torch.einsum("ki, ki -> ", self.single_filtering_probs[:, 0, :], alpha_1[:, 0, :])
+            cross_ent_pos_0 = torch.einsum("ki, ki -> ", self.single_filtering_probs[:, 0, :], torch.log(alpha_1[:, 0, :]))
             cross_ent_pos_2_to_M = torch.einsum("kmij, kmij -> ", self.couple_filtering_probs, alpha_2)
             E_T += torch.exp(w_T_list[l] - normalising_weight) * (cross_ent_pos_0 + cross_ent_pos_2_to_M)
 
-        elbo = E_T + self.marginal_entropy()
+        return E_T
+
+    def elbo(self, T_list, w_T_list, q_eps: Union['qEpsilon', 'qEpsilonMulti']) -> float:
+        # unique_arcs, unique_arcs_count = tree_utils.get_unique_edges(T_list, self.config.n_nodes)
+        # for (a, a_count) in zip(unique_arcs, unique_arcs_count):
+        # alpha_1, alpha_2 = self.exp_alpha()
+
+        elbo = self.cross_entropy(T_list, w_T_list, q_eps) + self.marginal_entropy()
         return elbo
 
     def update(self, obs: torch.Tensor,

tests/tests_VICtree_fixed_tree.py
@@ -57,10 +57,10 @@ class VICtreeFixedTreeTestCase(unittest.TestCase):
         copy_tree.run(10)
 
     def test_large_tree(self):
-        K = 3
+        K = 5
         tree = tests.utils_testing.get_tree_K_nodes_random(K)
-        n_cells = 20
-        n_sites = 10
+        n_cells = 200
+        n_sites = 100
         n_copy_states = 7
         data = torch.ones((n_sites, n_cells))
         C, y, z, pi, mu, sigma2, eps = self.simul_data_pyro(data, n_cells, n_sites, n_copy_states, tree)
@@ -69,10 +69,11 @@ class VICtreeFixedTreeTestCase(unittest.TestCase):
         qc, qt, qeps, qz, qpi, qmt = self.set_up_q(config)
         p = GenerativeModel(config, tree)
         q = VarDistFixedTree(config, qc, qz, qeps, qmt, qpi, tree, y)
-        q.initialize()
         copy_tree = CopyTree(config, p, q, y)
 
         copy_tree.run(10)
         q_C = copy_tree.q.c.single_filtering_probs
-        q_pi = copy_tree.q.pi.concentration_param
-        print(f"True pi: {pi} \n q(pi): {q_pi}")
+        q_pi = copy_tree.q.z.pi
+        delta = copy_tree.q.pi.concentration_param
+        print(f"True pi: {pi} \n variational concentration param: {delta}")
+        print(f"True C: {C[1, 5:10]} \n q(C): {q_C[1, 5:10, :]}")