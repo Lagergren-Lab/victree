[fix] normalized eta in qcupdate and switched to log-probs in compute marginals

src/variational_distributions/var_dists.py
@@ -145,9 +145,10 @@ class qC(VariationalDistribution):
             new_eta1 = new_eta1 + tree_eta1 * weight
             new_eta2 = new_eta2 + tree_eta2 * weight
 
-        self.eta1 = new_eta1
-        self.eta2 = new_eta2
-        # FIXME: eta1 and eta2 don't come out normalized (in exp scale)
+        # eta1 and eta2 don't come out normalized (in exp scale)
+        # need normalization
+        self.eta1[...] = new_eta1 - torch.logsumexp(new_eta1, dim=-1, keepdim=True)
+        self.eta2[...] = new_eta2 - torch.logsumexp(new_eta2, dim=-1, keepdim=True)
         # update the filtering probs
         self.compute_filtering_probs()
         return super().update()
@@ -181,8 +182,8 @@ class qC(VariationalDistribution):
         root = sorted_nodes[0]
         inner_nodes = sorted_nodes[1:]
 
-        e_eta1 = torch.zeros(self.eta1.shape)
-        e_eta2 = torch.zeros(self.eta2.shape)
+        e_eta1 = torch.empty_like(self.eta1)
+        e_eta2 = torch.empty_like(self.eta2)
 
         # eta_1_iota(1, i)
         e_eta1[inner_nodes, :] = torch.einsum('pj,ij->pi',
@@ -195,7 +196,7 @@ class qC(VariationalDistribution):
                               q_mutau.exp_log_emission(obs))
         # eta_2_kappa(m, i, i')
         if not isinstance(q_eps, qEpsilonMulti):
-            e_eta2 = torch.einsum('pmjk,hikj,pmh->pmih',
+            e_eta2[...] = torch.einsum('pmjk,hikj,pmh->pmih',
                                               self.couple_filtering_probs,
                                               q_eps.exp_zipping(),
                                               e_eta1_m[:, 1:, :])
@@ -219,8 +220,8 @@ class qC(VariationalDistribution):
     def exp_alpha(self, tree: nx.DiGraph, q_eps: Union['qEpsilon', 'qEpsilonMulti']) -> Tuple[
         torch.Tensor, torch.Tensor]:
 
-        e_alpha1 = torch.zeros(self.eta1.shape)
-        e_alpha2 = torch.zeros(self.eta2.shape)
+        e_alpha1 = torch.empty_like(self.eta1)
+        e_alpha2 = torch.empty_like(self.eta2)
 
         # alpha_iota(m, i)
         # as in the write-up, then it's split and e_alpha12[:, 1:, :] is
@@ -261,28 +262,25 @@ class qC(VariationalDistribution):
 
     def compute_filtering_probs(self):
         # shape K x S (K is batch size / clones)
-        initial_probs = torch.exp(self.eta1)
+        initial_log_probs = self.eta1
         # shape K x M x S x S
-        transition_probs = torch.exp(self.eta2)
-        if self.config.debug:
-            assert(np.allclose(initial_probs.sum(dim=1), 1.))
-            assert(np.allclose(transition_probs.sum(dim=3), 1.))
+        transition_log_probs = self.eta2
 
-        single = torch.empty(self.single_filtering_probs.shape)
-        couple = torch.empty(self.couple_filtering_probs.shape)
-        single[:, 0, :] = initial_probs
+        log_single = torch.empty(self.single_filtering_probs.shape)
+        log_couple = torch.empty(self.couple_filtering_probs.shape)
+        log_single[:, 0, :] = initial_log_probs
         for m in range(self.config.chain_length - 1):
             # first compute the two slice P(X_m, X_m+1) = P(X_m)P(X_m+1|X_m)
-            couple[:, m, ...] = single[:, m, :, None] * transition_probs[:, m, ...]
+            log_couple[:, m, ...] = log_single[:, m, :, None] + transition_log_probs[:, m, ...]
             # then marginalize over X_m to obtain P(X_m+1)
-            single[:, m + 1, :] = torch.sum(couple[:, m, ...], dim=1)
+            log_single[:, m + 1, :] = torch.logsumexp(log_couple[:, m, ...], dim=1)
 
         if self.config.debug:
-            assert(np.allclose(single.sum(dim=2), 1.))
-            assert(np.allclose(couple.sum(dim=(2, 3)), 1.))
+            assert(np.allclose(log_single.logsumexp(dim=2).exp(), 1.))
+            assert(np.allclose(log_couple.logsumexp(dim=(2, 3)).exp(), 1.))
 
-        self.single_filtering_probs = single
-        self.couple_filtering_probs = couple
+        self.single_filtering_probs = torch.exp(log_single)
+        self.couple_filtering_probs = torch.exp(log_couple)
         return self.single_filtering_probs, self.couple_filtering_probs
 
     def calculate_filtering_probs(self):

tests/test_qc.py
@@ -25,7 +25,7 @@ class qCTestCase(unittest.TestCase):
     def test_update(self):
         # design simple test: fix all other variables
         # and update the q_c params
-        cells_per_clone = 100
+        cells_per_clone = 10
         cfg = Config(n_nodes=3, n_states=5, n_cells=3 * cells_per_clone, chain_length=10,
                      wis_sample_size=2, debug=True)
         qc = qC(cfg)
@@ -38,6 +38,9 @@ class qCTestCase(unittest.TestCase):
             [[200] * 4 + [300] * 6] * cells_per_clone +
             [[100] * 8 + [200] * 2] * cells_per_clone
         ).T
+        # introduce some randomness
+        obs += torch.distributions.normal.Normal(0, 10).sample(obs.shape).int()
+
         self.assertEqual(obs.shape, (cfg.chain_length, cfg.n_cells))
         # fix epsilon to be with mean 1/9 and low variance
         # both clones only have one asymmetric transition out of 9
@@ -59,7 +62,7 @@ class qCTestCase(unittest.TestCase):
         trees = [fix_tree] * cfg.wis_sample_size
         wis_weights = [1/cfg.wis_sample_size] * cfg.wis_sample_size
 
-        for i in range(100):
+        for i in range(10):
             qc.update(obs, fix_qeps, fix_qz, fix_qmt,
                       trees=trees, tree_weights=wis_weights)
             # print(qc.elbo(trees, wis_weights, fix_qeps))
@@ -70,11 +73,12 @@ class qCTestCase(unittest.TestCase):
 
         print(qc.single_filtering_probs)
         print(qc.couple_filtering_probs)
-        # self.assertGreater(qc.couple_filtering_probs[1, 3, 2, 3], qc.couple_filtering_probs[1, 3, 2, 2])
-        # self.assertGreater(qc.couple_filtering_probs[1, 3, 2, 3], qc.couple_filtering_probs[1, 5, 2, 3])
 
-        # self.assertGreater(qc.couple_filtering_probs[2, 7, 1, 2], qc.couple_filtering_probs[2, 7, 1, 1])
-        # self.assertGreater(qc.couple_filtering_probs[2, 7, 1, 2], qc.couple_filtering_probs[2, 4, 1, 2])
+        self.assertGreater(qc.couple_filtering_probs[1, 3, 2, 3], qc.couple_filtering_probs[1, 3, 2, 2])
+        self.assertGreater(qc.couple_filtering_probs[1, 3, 2, 3], qc.couple_filtering_probs[1, 5, 2, 3])
+
+        self.assertGreater(qc.couple_filtering_probs[2, 7, 1, 2], qc.couple_filtering_probs[2, 7, 1, 1])
+        self.assertGreater(qc.couple_filtering_probs[2, 7, 1, 2], qc.couple_filtering_probs[2, 4, 1, 2])
 
     def test_filtering_probs_update(self):
 