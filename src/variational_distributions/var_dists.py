[fix] added new muTau elbo computation

src/variational_distributions/var_dists.py
@@ -1274,7 +1274,7 @@ Initialize the mu and tau params given observations
         CE_prior = self.alpha_0 * torch.log(self.beta_0) + 0.5 * torch.log(self.lmbda_0) - torch.lgamma(self.alpha_0)
         CE_constants = 0.5 * torch.log(torch.tensor(2 * torch.pi))
         CE_var_terms = self.exp_log_tau()
-        CE_cross_terms = self.beta_0 * self.exp_tau() + (self.alpha_0 - 1) * self.exp_log_tau() - \
+        CE_cross_terms = - self.beta_0 * self.exp_tau() + (self.alpha_0 - 1) * self.exp_log_tau() - \
                          0.5 * self.lmbda_0 / self.lmbda
         CE_arr = CE_constants + CE_prior + CE_var_terms + CE_cross_terms
         return torch.sum(CE_arr)
@@ -1290,6 +1290,23 @@ Initialize the mu and tau params given observations
 
     def elbo(self) -> float:
         return self.cross_entropy() - self.entropy()
+
+    def elbo_alt(self):
+        exp_entropy_mun = .5 * (1. + torch.log(2 * np.pi * self.beta) - torch.log(self.lmbda) -
+                                torch.digamma(self.alpha))
+        entropy_taun = self.alpha - self.beta.log() + torch.lgamma(self.alpha) +\
+                       (1 - self.alpha) * torch.digamma(self.alpha)
+        entropy = exp_entropy_mun + entropy_taun
+
+        cross_entropy = .5 * (torch.tensor(2 * torch.pi).log() - self.lmbda.log() + self.lmbda_0 / self.lmbda) -\
+            self.alpha_0 * (torch.digamma(self.alpha) - self.beta.log()) - self.alpha_0 * self.beta_0.log() +\
+            torch.lgamma(self.alpha_0) + self.beta_0 * self.alpha / self.beta
+
+        if self.config.debug:
+            assert entropy.shape == (self.config.n_cells, )
+            assert cross_entropy.shape == (self.config.n_cells, )
+
+        return - torch.sum(entropy + cross_entropy)
 
     def exp_log_emission(self, obs: torch.Tensor) -> torch.Tensor:
         out_shape = (self.config.n_cells, self.config.chain_length, self.config.n_states)

tests/test_qmt.py
@@ -100,8 +100,11 @@ class qmtTestCase(unittest.TestCase):
 
     def test_entropy(self):
         obs = torch.randint(low=10, high=20, size=(self.config.chain_length, self.config.n_cells), dtype=torch.float)
-        self.qc.single_filtering_probs = torch.zeros((self.K, self.M, self.A))
-        self.qc.single_filtering_probs[:, :, 1] = 2
+        eps = 1e-5
+        self.qc.single_filtering_probs = torch.zeros((self.K, self.M, self.A)) + eps
+        self.qc.single_filtering_probs[:, :, 1] = 1 - eps
         self.qmt.update(qc=self.qc, qz=self.qz, obs=obs)
         elbo_qmt = self.qmt.elbo()
+        alt_elbo_qmt = self.qmt.elbo_alt()
         print(f"ELBO(mu, tau): {elbo_qmt}")
+        print(f"new ELBO(mu, tau): {alt_elbo_qmt}")