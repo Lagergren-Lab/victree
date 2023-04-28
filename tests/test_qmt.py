749c1b9d2f1c6589e10c246c0196dee0b65651c3
Harald Melin <haraldme@kth.se>
2023-04-28 14:35:37 -0400

    fixed qmt test in test_updates

--- tests/test_qmt.py
+++ tests/test_qmt.py
@@ -154,6 +154,6 @@ class qmtTestCase(unittest.TestCase):
             elbo_qmt = qmt.elbo()
             print(f"[{i}] old ELBO(mu, tau): {elbo_qmt:.2f}")
             print(f"[{i}]" + str(qmt))
-            qmt.update(,
+            qmt.update(joint_q.c, joint_q.z, joint_q.obs)
 
 
