58198fe0c4017a8a11b973b2685857cb9a48243c
Harald Melin <haraldme@kth.se>
2023-05-01 14:56:58 -0400

    diagnostics init fix

--- tests/test_qT.py
+++ tests/test_qT.py
@@ -12,21 +12,21 @@ from variational_distributions.var_dists import qEpsilonMulti, qT, qC, qEpsilon
 class qTTestCase(unittest.TestCase):
 
     def setUp(self) -> None:
-        self.qt5: qT = qT(config=Config(n_nodes=5)).initialize()
-        pass
+        self.qt5: qT = qT(config=Config(n_nodes=5))
 
     def test_q_T_running_for_two_simple_Ts_random_qC(self):
         M = 20
         A = 5
-        N = 3
-        self.config = Config(n_nodes=N, n_states=A, chain_length=M)
-        self.q_T = qT(config=self.config)
+        N = 5
+        config = Config(n_nodes=N, n_states=A, chain_length=M)
+        q_T = qT(config=config)
         T_list, q_C_pairwise_marginals = utils_testing.get_two_simple_trees_with_random_qCs(M, A, N)
-        q_C = qC(config=self.config)
+        q_C = qC(config=config)
         q_C.couple_filtering_probs = q_C_pairwise_marginals
-        q_epsilon = qEpsilonMulti(config=self.config)
+        q_epsilon = qEpsilonMulti(config=config)
+        q_epsilon.initialize()
         # Act
-        log_q_T = self.q_T.update_CAVI(T_list, q_C, q_epsilon)
+        log_q_T = q_T.update_CAVI(T_list, q_C, q_epsilon)
 
         # Assert
         print(f"log_q_T of T_1 and T_2: {log_q_T}")
