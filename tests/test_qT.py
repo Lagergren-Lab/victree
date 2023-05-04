dd2cda26d4cf499bb73bc8691858a44e0d40bd8e
Vittorio Zampinetti <zampinetti@gmail.com>
2023-05-04 12:19:40 -0400

    [test] quality of inferred trees

--- tests/test_qT.py
+++ tests/test_qT.py
@@ -4,7 +4,7 @@ import numpy as np
 import torch
 
 from tests import utils_testing
-from utils.config import Config
+from utils.config import Config, set_seed
 from utils.tree_utils import tree_to_newick
 from variational_distributions.var_dists import qEpsilonMulti, qT, qC, qEpsilon
 
@@ -12,6 +12,7 @@ from variational_distributions.var_dists import qEpsilonMulti, qT, qC, qEpsilon
 class qTTestCase(unittest.TestCase):
 
     def setUp(self) -> None:
+        set_seed(42)
         self.qt5: qT = qT(config=Config(n_nodes=5))
 
     def test_q_T_running_for_two_simple_Ts_random_qC(self):
@@ -71,6 +72,38 @@ class qTTestCase(unittest.TestCase):
         print(f"TOP {k} sampled trees")
         for t_str, lw in zip(topk_sampled_str, sampled_lw):
             print(f"{t_str}: {lw.exp()}")
+
+    def test_inference_fixed_ceps(self):
+        # build ad-hoc c
+        c = torch.tensor([
+            [2] * 20,
+            [3] * 10 + [2] * 10,
+            [2] * 5 + [3] * 5 + [2] * 10,
+            [2] * 3 + [3] * 3 + [1] * 3 + [2] * 3 + [3] * 3 + [2] * 5
+        ])
+        config = Config(4, n_states=7, eps0=1e-2, chain_length=c.shape[1],
+                        wis_sample_size=100, step_size=.2, debug=True)
+
+        qt = qT(config).initialize()
+        qeps = qEpsilonMulti(config).initialize()
+        qc = qC(config, true_params={'c': c})
+
+        for i in range(10):
+            qt.update(qc, qeps)
+            t, w = qt.get_trees_sample()
+            qeps.update(t, w, qc)
+
+        # print(qc)
+        # print(qt)
+        # print(qeps)
+        gt_tree_newick = "((2)1,3)0"
+        tol = 3
+        qt_pmf = qt.get_pmf_estimate()
+        sorted_newick = sorted(qt_pmf, key=qt_pmf.get, reverse=True)
+        self.assertTrue(gt_tree_newick in sorted_newick[:tol],
+                        msg=f"true " + gt_tree_newick + f" not in the first {tol} trees. those are:"
+                                                        f"{sorted_newick[0]} | {sorted_newick[1]} | {sorted_newick[2]}")
+
 
 
 
