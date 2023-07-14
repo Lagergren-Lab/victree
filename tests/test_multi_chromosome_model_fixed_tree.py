ff4393372f083e5a7337a8a0eab2f9030231dc2d
Harald Melin <haraldme@kth.se>
2023-07-14 11:04:34 -0400

    comparison test of qC and qCmultichrome added

--- tests/test_multi_chromosome_model_fixed_tree.py
+++ tests/test_multi_chromosome_model_fixed_tree.py
@@ -85,6 +85,59 @@ class MultiChromosomeTestCase(unittest.TestCase):
         torch.manual_seed(0)
         tree = tests.utils_testing.get_tree_three_nodes_balanced()
         K = len(tree.nodes)
+        N = 100
+        M = 200
+        chromosome_indexes = [int(M / 10), int(M / 10 * 5), int(M / 10 * 8)]
+        n_chromosomes = len(chromosome_indexes) + 1
+        A = 7
+        dir_alpha = torch.tensor([1., 3., 3.])
+        nu_0 = torch.tensor(1.)
+        lambda_0 = torch.tensor(5.)
+        alpha0 = torch.tensor(50.)
+        beta0 = torch.tensor(5.)
+        a0 = torch.tensor(10.0)
+        b0 = torch.tensor(800.0)
+        eps0_a = torch.tensor(10.0)
+        eps0_b = torch.tensor(800.0)
+        config = Config(n_nodes=K, n_states=A, n_cells=N, chain_length=M,
+                        chromosome_indexes=chromosome_indexes, n_chromosomes=n_chromosomes,
+                        step_size=0.3,
+                        debug=False, diagnostics=False)
+        model = MultiChromosomeGenerativeModel(config)
+        out_simul = model.simulate_data(tree, a0=a0, b0=b0, eps0_a=eps0_a, eps0_b=eps0_b, delta=dir_alpha,
+                                        nu0=nu_0, lambda0=lambda_0, alpha0=alpha0, beta0=beta0)
+        y = out_simul['obs']
+        c = out_simul['c']
+        z = out_simul['z']
+        pi = out_simul['pi']
+        mu = out_simul['mu']
+        tau = out_simul['tau']
+        eps = out_simul['eps']
+        eps0 = out_simul['eps0']
+
+        print(f"Epsilon: {eps}")
+        print(f"Mu in range: [{mu.min()}, {mu.max()}] ")
+
+        qc, qt, qeps, qz, qpi, qmt = self.set_up_q(config)
+
+        q = FixedTreeJointDist(config, qc, qz, qeps, qmt, qpi, tree, y)
+        # initialize all var dists
+        q.initialize()
+        copy_tree = VICTree(config, q, y)
+
+        copy_tree.run(n_iter=50)
+
+        # Assert
+        torch.set_printoptions(precision=2)
+        model_variational_comparisons.fixed_T_comparisons(obs=y, true_C=c, true_Z=z, true_pi=pi, true_mu=mu,
+                                                          true_tau=tau, true_epsilon=eps, q_c=copy_tree.q.c,
+                                                          q_z=copy_tree.q.z, qpi=copy_tree.q.pi, q_mt=copy_tree.q.mt)
+
+
+    def test_qC_vs_qCMutliChrom_random_node_tree(self):
+        torch.manual_seed(0)
+        K = 5
+        tree = tests.utils_testing.get_tree_K_nodes_random(K)
         N = 1000
         M = 2000
         chromosome_indexes = [int(M / 10), int(M / 10 * 5), int(M / 10 * 8)]
@@ -116,6 +169,8 @@ class MultiChromosomeTestCase(unittest.TestCase):
         eps0 = out_simul['eps0']
 
         print(f"Epsilon: {eps}")
+        print(f"Mu in range: [{mu.min()}, {mu.max()}]")
+        print(f"tau in range: [{tau.min()}, {tau.max()}]")
 
         qc, qt, qeps, qz, qpi, qmt = self.set_up_q(config)
 
@@ -124,7 +179,15 @@ class MultiChromosomeTestCase(unittest.TestCase):
         q.initialize()
         copy_tree = VICTree(config, q, y)
 
-        copy_tree.run(n_iter=50)
+        copy_tree.run(n_iter=100)
+
+        qc2, qt2, qeps2, qz2, qpi2, qmt2 = self.set_up_q(config)
+        qc2 = qC(config)
+        q2 = FixedTreeJointDist(config, qc2, qz2, qeps2, qmt2, qpi2, tree, y)
+        q2.initialize()
+        copy_tree2 = VICTree(config, q2, y)
+
+        copy_tree2.run(n_iter=100)
 
         # Assert
         torch.set_printoptions(precision=2)
@@ -132,35 +195,53 @@ class MultiChromosomeTestCase(unittest.TestCase):
                                                           true_tau=tau, true_epsilon=eps, q_c=copy_tree.q.c,
                                                           q_z=copy_tree.q.z, qpi=copy_tree.q.pi, q_mt=copy_tree.q.mt)
 
+        # Assert
+        torch.set_printoptions(precision=2)
+        model_variational_comparisons.fixed_T_comparisons(obs=y, true_C=c, true_Z=z, true_pi=pi, true_mu=mu,
+                                                          true_tau=tau, true_epsilon=eps, q_c=copy_tree2.q.c,
+                                                          q_z=copy_tree2.q.z, qpi=copy_tree2.q.pi, q_mt=copy_tree2.q.mt)
+
     def test_large_tree(self):
         torch.manual_seed(0)
         K = 9
         tree = tests.utils_testing.get_tree_K_nodes_random(K)
-        n_cells = 500
-        n_sites = 500
-        n_copy_states = 5
-        dir_alpha0 = 10.
+        N = 1000
+        M = 2000
+        chromosome_indexes = [int(M / 10), int(M / 10 * 5), int(M / 10 * 8)]
+        n_chromosomes = len(chromosome_indexes) + 1
+        A = 7
+        dir_alpha = torch.tensor([1., 3., 3.])
         nu_0 = torch.tensor(1.)
-        lambda_0 = torch.tensor(10.)
-        alpha0 = torch.tensor(500.)
-        beta0 = torch.tensor(50.)
+        lambda_0 = torch.tensor(5.)
+        alpha0 = torch.tensor(50.)
+        beta0 = torch.tensor(5.)
         a0 = torch.tensor(10.0)
-        b0 = torch.tensor(200.0)
-        y, C, z, pi, mu, tau, eps, eps0 = simulate_full_dataset_no_pyro(n_cells, n_sites, n_copy_states, tree,
-                                                                        nu_0=nu_0,
-                                                                        lambda_0=lambda_0, alpha0=alpha0, beta0=beta0,
-                                                                        a0=a0, b0=b0, dir_alpha0=dir_alpha0)
-        print(f"C: {C}")
-        # print(f"z: {z}")
-        # print(f"y: {y}")
-        # print(f"mu: {mu}")
+        b0 = torch.tensor(800.0)
+        eps0_a = torch.tensor(10.0)
+        eps0_b = torch.tensor(800.0)
+        config = Config(n_nodes=K, n_states=A, n_cells=N, chain_length=M,
+                        chromosome_indexes=chromosome_indexes, n_chromosomes=n_chromosomes,
+                        step_size=0.3,
+                        debug=False, diagnostics=False)
+        model = MultiChromosomeGenerativeModel(config)
+        out_simul = model.simulate_data(tree, a0=a0, b0=b0, eps0_a=eps0_a, eps0_b=eps0_b, delta=dir_alpha,
+                                        nu0=nu_0, lambda0=lambda_0, alpha0=alpha0, beta0=beta0)
+        y = out_simul['obs']
+        c = out_simul['c']
+        z = out_simul['z']
+        pi = out_simul['pi']
+        mu = out_simul['mu']
+        tau = out_simul['tau']
+        eps = out_simul['eps']
+        eps0 = out_simul['eps0']
+
+        print(f"Epsilon: {eps}")
+        print(f"Mu in range: [{mu.min()}, {mu.max()}] ")
 
         out_dir = "./test_output/" + self._testMethodName
         if not os.path.exists(out_dir):
             os.makedirs(out_dir)
 
-        config = Config(n_nodes=K, n_states=n_copy_states, n_cells=n_cells, chain_length=n_sites, step_size=0.1,
-                        diagnostics=False, out_dir=out_dir, annealing=1.)
         test_dir_name = tests.utils_testing.create_test_output_catalog(config, self._testMethodName)
         qc, qt, qeps, qz, qpi, qmt = self.set_up_q(config)
         q = FixedTreeJointDist(config, qc, qz, qeps, qmt, qpi, tree, y)
@@ -172,7 +253,7 @@ class MultiChromosomeTestCase(unittest.TestCase):
         print(q.c)
 
         # Assert
-        model_variational_comparisons.fixed_T_comparisons(obs=y, true_C=C, true_Z=z, true_pi=pi, true_mu=mu,
+        model_variational_comparisons.fixed_T_comparisons(obs=y, true_C=c, true_Z=z, true_pi=pi, true_mu=mu,
                                                           true_tau=tau, true_epsilon=eps, q_c=copy_tree.q.c,
                                                           q_z=copy_tree.q.z, qpi=copy_tree.q.pi, q_mt=copy_tree.q.mt)
 
