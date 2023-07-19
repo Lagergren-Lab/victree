fixed bug in qMultiEpsilon partial ELBO calculation

src/variational_distributions/var_dists.py
@@ -1335,7 +1335,7 @@ class qEpsilonMulti(VariationalDistribution):
                                                            range(config.n_nodes)) if v != 0 and u != v]
         self._alpha_dict = {e: torch.empty(1) for e in gedges}
         self._beta_dict = {e: torch.empty(1) for e in gedges}
-
+        self._gedges = gedges
         if true_params is not None:
             assert "eps" in true_params
         self.true_params = true_params
@@ -1360,6 +1360,10 @@ class qEpsilonMulti(VariationalDistribution):
     def beta_dict(self, b: dict):
         for e, w in b.items():
             self._beta_dict[e] = w
+
+    @property
+    def gedges(self):
+        return self._gedges
 
     @property
     def alpha(self):
@@ -1473,15 +1477,15 @@ class qEpsilonMulti(VariationalDistribution):
                 anti_sym_mask[i, j, k, l] = 1
         return co_mut_mask, anti_sym_mask
 
-    def cross_entropy(self, T_eval, w_T_eval):
+    def neg_cross_entropy(self, T_eval, w_T_eval):
         a = self.alpha_dict
         a_0 = self.alpha_prior
         b = self.beta_dict
         b_0 = self.beta_prior
-        tot_H = 0
+        tot_CE = 0
         unique_edges, unique_edges_count = tree_utils.get_unique_edges(T_eval, self.config.n_nodes)
+        H_edge = torch.zeros((self.config.n_nodes, self.config.n_nodes))
         for (u, v) in unique_edges:
-            n_uv = unique_edges_count[u, v]
             a_uv = a[u, v]
             a_uv_0 = a_0
             b_uv = b[u, v]
@@ -1491,18 +1495,23 @@ class qEpsilonMulti(VariationalDistribution):
             psi_a_uv = torch.digamma(a_uv)
             psi_b_uv = torch.digamma(b_uv)
             psi_a_plus_b_uv = torch.digamma(a_uv + b_uv)
-            tot_H += torch.sum(n_uv * (log_Beta_ab_0_uv - (a_uv_0 - 1) * (psi_a_uv - psi_a_plus_b_uv) -
+
+            H_edge[u, v] += torch.sum((log_Beta_ab_0_uv - (a_uv_0 - 1) * (psi_a_uv - psi_a_plus_b_uv) -
                                        (b_uv_0 - 1) * (psi_b_uv - psi_a_plus_b_uv)))
-        return tot_H
+
+        for (T, w) in zip(T_eval, w_T_eval):
+            for e in T.edges:
+                tot_CE += H_edge[e] * w
+
+        return -tot_CE
 
     def entropy(self, T_eval, w_T_eval):
         a = self.alpha_dict
         b = self.beta_dict
         tot_H = 0
-        # TODO: replace n_uv by weights*n_uv
         unique_edges, unique_edges_count = tree_utils.get_unique_edges(T_eval, self.config.n_nodes)
+        H_edge = torch.zeros((self.config.n_nodes, self.config.n_nodes))
         for (u, v) in unique_edges:
-            n_uv = unique_edges_count[u, v]
             a_uv = a[u, v]
             b_uv = b[u, v]
             a_b_uv_tens = torch.tensor((a_uv, b_uv))
@@ -1510,13 +1519,17 @@ class qEpsilonMulti(VariationalDistribution):
             psi_a_uv = torch.digamma(a_uv)
             psi_b_uv = torch.digamma(b_uv)
             psi_a_plus_b_uv = torch.digamma(a_uv + b_uv)
-            tot_H += torch.sum(n_uv * (log_Beta_ab_uv - (a_uv - 1) * psi_a_uv - (b_uv - 1) * psi_b_uv +
+            H_edge[u, v] = torch.sum((log_Beta_ab_uv - (a_uv - 1) * psi_a_uv - (b_uv - 1) * psi_b_uv +
                                        (a_uv + b_uv - 2) * psi_a_plus_b_uv))
+
+        for (T, w) in zip(T_eval, w_T_eval):
+            for e in T.edges:
+                tot_H += H_edge[e] * w
         return tot_H
 
     def compute_elbo(self, T_eval, w_T_eval) -> float:
         entropy_eps = self.entropy(T_eval, w_T_eval)
-        CE_eps = self.cross_entropy(T_eval, w_T_eval)
+        CE_eps = self.neg_cross_entropy(T_eval, w_T_eval)
         return CE_eps + entropy_eps
 
     def h_eps0(self, i: Optional[int] = None, j: Optional[int] = None) -> Union[float, torch.Tensor]:

tests/test_q_epsilon.py
@@ -26,7 +26,7 @@ class qEpsilonTestCase(unittest.TestCase):
 
         config = Config(n_nodes=N, n_states=A, chain_length=M)
 
-        T_list, q_C_pairwise_marginals = utils_testing.get_two_simple_trees_with_random_qCs(M, A, N)
+        T_list, q_C_pairwise_marginals = utils_testing.get_random_q_C(M, A)
         w_T = torch.tensor([0.3, 0.7])
 
         qc = qC(config)
@@ -56,12 +56,14 @@ class qEpsilonTestCase(unittest.TestCase):
     def test_q_epsilon_ELBO_larger_for_no_mutation(self):
         # Arange
         K = 2
-        M = 2
+        M = 10
         A = 5
 
         config = Config(n_nodes=K, n_states=A, chain_length=M)
-        q_eps1 = qEpsilonMulti(config, alpha_prior=1., beta_prior=10.)
-        q_eps2 = qEpsilonMulti(config, alpha_prior=1., beta_prior=10.)
+        q_eps1 = qEpsilonMulti(config, alpha_prior=1., beta_prior=100.)
+        q_eps2 = qEpsilonMulti(config, alpha_prior=1., beta_prior=100.)
+        q_eps1.initialize("non_mutation")
+        q_eps2.initialize("non_mutation")
         T = nx.DiGraph()
         T.add_edge(0, 1)
         w_T = [1.]
@@ -73,10 +75,12 @@ class qEpsilonTestCase(unittest.TestCase):
         q_C2.couple_filtering_probs[1] = torch.rand((A, A))
 
         # Act
-        self.assertTrue(q_eps1.compute_elbo([T], w_T) == q_eps2.compute_elbo([T], w_T))
+        q1_partial_elbo = q_eps1.compute_elbo([T], w_T)
+        q2_partial_elbo = q_eps2.compute_elbo([T], w_T)
+        self.assertEqual(q1_partial_elbo, q2_partial_elbo)
 
         q_eps1.update([T], w_T, q_C1)
-        q_eps1.update([T], w_T, q_C2)
+        q_eps2.update([T], w_T, q_C2)
         elbo_1 = q_eps1.compute_elbo([T], w_T)
         elbo_2 = q_eps2.compute_elbo([T], w_T)
 