bc7fb3e6cafa4ea771dd6475a09d640eb2d0287b
Harald Melin <haraldme@kth.se>
2023-09-15 10:17:07 +0200

    fixed tests

--- tests/test_inference/test_VICTree_clonal_vs_subclonal_profiles_fixed_tree.py
+++ tests/test_inference/test_VICTree_clonal_vs_subclonal_profiles_fixed_tree.py
@@ -42,7 +42,7 @@ def generate_clonal_profile_data(A, C, K, M_clonal, M_subclonal, N, eps, mu, tau
     return M_tot, c_tot, eps_tot, y_tot, c_clonal
 
 
-#@unittest.skip('long exec test')
+@unittest.skip('long exec test')
 class VICtreeClonalVsSubclonalProfilesFixedTreeTestCase(unittest.TestCase):
 
     def set_up_q(self, config):
@@ -103,7 +103,7 @@ class VICtreeClonalVsSubclonalProfilesFixedTreeTestCase(unittest.TestCase):
                                 diagnostics=False, annealing=1.)
 
         test_dir_name = tests.utils_testing.create_test_output_catalog(config_general, self.id().replace(".", "/"),
-                                                                       base_dir='./../test_output')
+                                                                       base_dir='./test_output')
 
         qc2, qt2, qeps2, qz2, qpi2, qmt2 = self.set_up_q(config_general)
         q2 = FixedTreeJointDist(y_tot, config_general, qc2, qz2, qeps2, qmt2, qpi2, tree)
@@ -189,7 +189,7 @@ class VICtreeClonalVsSubclonalProfilesFixedTreeTestCase(unittest.TestCase):
 
         # Assert
         test_dir_name = tests.utils_testing.create_test_output_catalog(config_init2, self.id().replace(".", "/"),
-                                                                       base_dir='./../test_output')
+                                                                       base_dir='./test_output')
         torch.set_printoptions(precision=2)
         out2 = model_variational_comparisons.fixed_T_comparisons(obs=y_tot, true_C=c_tot, true_Z=z, true_pi=pi,
                                                                  true_mu=mu,
